83fd00990685defbd39bf8809a5e6480499cea1a
Angela Weber <angelaweber95@hotmail.com>
2020-02-10 22:12:18 -0800

    add styling for flex in post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,13 +5,29 @@ function Post(){
       padding: "0px 10px",
       width: "200px",
       height: "45px",
-      display: "flex",
-      textAlign: "left"
+      textAlign: "left",
+      flexGrow: "1",
+      maxWidth: "350px",
+      border: "2px solid #00acee",
+      color: "#00acee"
     }
+  var postContainer = {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "20px"
+  }
+
+  var postIcon = {
+    border: "1px solid black",
+    height: "45px",
+    width: "45px",
+    backgroundColor: "teal"
+  }
 
   return (
-    <div>
-      <input  style={postStyle} type="text" placeholder="What's happening?" />
+    <div style={postContainer}>
+      <div style={postIcon}></div>
+      <input style={postStyle} type="text" placeholder="What's happening?" />
     </div>
   );
 }
